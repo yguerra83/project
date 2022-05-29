96e170a7b1a56fb0a6704bea34ebf2a923242072
Yannier Guerra <yguerra83@gmail.com>
2022-05-29 14:03:44 -0400

    some changes

--- javascript-file.js
+++ javascript-file.js
@@ -1,8 +1,29 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
+document.addEventListener("DOMContentLoaded", function () {
+            fields.firstName = document.getElementById('firstName');
+            fields.lastName = document.getElementById('lastName');
+            fields.email = document.getElementById('email');
+            fields.question = document.getElementById('question');
+        }
+
+        function isNotEmpty(value) {
+            if (value == null || typeof value == 'undefined') return false;
+            return (value.length > 0);
+        }
+
+        function isEmail(email) {
+            let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+            return regex.test(String(email).toLowerCase());
+        }
+
+        function fieldValidation(field, validationFunction) {
+            if (field == null) return false;
+
+            let isFieldValid = validationFunction(field.value)
+            if (!isFieldValid) {
+                field.className = 'placeholderRed';
+            } else {
+                field.className = '';
+            }
+
+            return isFieldValid;
+        }
