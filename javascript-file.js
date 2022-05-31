e1f31ee9162c6c26a1dc519a0882b7591e3ca102
Yannier Guerra <yguerra83@gmail.com>
2022-05-30 21:10:01 -0400

    Final changes to the project

--- javascript-file.js
+++ javascript-file.js
@@ -1,62 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-            fields.firstName = document.getElementById('firstName');
-            fields.lastName = document.getElementById('lastName');
-            fields.email = document.getElementById('email');
-            fields.question = document.getElementById('question');
-        }
-
-        function isNotEmpty(value) {
-            if (value == null || typeof value == 'undefined') return false;
-            return (value.length > 0);
-        }
-
-        function isEmail(email) {
-            let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-            return regex.test(String(email).toLowerCase());
-        }
-
-        function fieldValidation(field, validationFunction) {
-            if (field == null) return false;
-
-            let isFieldValid = validationFunction(field.value)
-            if (!isFieldValid) {
-                field.className = 'placeholderRed';
-            } else {
-                field.className = '';
-            }
-
-            return isFieldValid;
-        }
-
-        function isValid() {
-            var valid = true;
-
-            valid &= fieldValidation(fields.firstName, isNotEmpty);
-            valid &= fieldValidation(fields.lastName, isNotEmpty);
-            valid &= fieldValidation(fields.email, isEmail);
-            valid &= fieldValidation(fields.question, isNotEmpty);
-            return valid;
-        }
-
-        class User {
-            constructor(firstName, lastName, gender, address, country, email, newsletter, question) {
-                this.firstName = firstName;
-                this.lastName = lastName;
-                this.gender = gender;
-                this.address = address;
-                this.country = country;
-                this.email = email;
-                this.newsletter = newsletter;
-                this.question = question;
-            }
-        }
-
-        function sendContact() {
-
-            if (isValid) {
-                let usr = new User(firstName.value, lastName.value, email.value)
-                alert('${usr.firstName} thank for registering.')
-            } else {
-                alert("There was an error")
-            }
-        }
+function ValidateEmail(inputText) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (inputText.value.match(mailformat)) {
+        alert("Valid email address!");
+        document.form1.text1.focus();
+        return true;
+    } else {
+        alert("You have entered an invalid email address!");
+        document.form1.text1.focus();
+        return false;
+    }
+}
