1fce8a477a16e93406f35cb0ed1a49693dec2e82
Yannier Guerra <yguerra83@gmail.com>
2022-06-02 00:21:49 -0400

    changes to javascript-file

--- javascript-file.js
+++ javascript-file.js
@@ -1,11 +1,11 @@
 function ValidateEmail(inputText) {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (inputText.value.match(mailformat)) {
-        alert("Valid email address!");
+        window.confirm("Valid email address!");
         document.form1.text1.focus();
         return true;
     } else {
-        alert("You have entered an invalid email address!");
+        window.confirm("You have entered an invalid email address!");
         document.form1.text1.focus();
         return false;
     }
